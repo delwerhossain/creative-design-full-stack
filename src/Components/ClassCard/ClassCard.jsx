e16a27da03e1747ea149a82544b7fe96a83558f9
delwerhossain <delwerhossain006@gmail.com>
2023-06-09 12:43:18 +0100

    Instructor Class page add

---
+++ src/Components/ClassCard/ClassCard.jsx
@@ -0,0 +1,100 @@
+import Rating from "react-rating";
+import "./classCard.scss";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+
+
+const ClassCard = ({ product, handleDeleteFilter }) => {
+  const {
+    _id,
+    name,
+    pictureURL,
+    instructorName,
+    instructorEmail,
+    subCategory,
+    price,
+    rating,
+    availableQuantity,
+    description,
+  } = product;
+  const { user } = useAuth();
+  const handleDelete = (id) => {
+    fetch(`https://server-toy-marketplace.vercel.app/toys/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          handleDeleteFilter(id);
+
+          Swal.fire({
+            title: "Delete",
+            text: "toys Delete Successfully",
+            showCancelButton: true,
+            icon: "delete",
+            confirmButtonText: "warning",
+          });
+        }
+      });
+  };
+  return (
+    <div className="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
+      <div className="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+        <div className="relative pb-96 overflow-hidden">
+          <img
+            className="absolute inset-0 w-full "
+            src={pictureURL}
+            alt={name}
+          />
+        </div>
+        <div className="p-4">
+          <span className="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold  tracking-wide text-xs">
+            Category - <span className="uppercase">{subCategory}</span>
+          </span>
+          <h2 className="mt-2 mb-2  font-bold">{name}</h2>
+          <p className="text-sm">{description}</p>
+          <div className="mt-3 flex items-center">
+            <span className="text-sm font-semibold">ab</span>&nbsp;
+            <span className="font-bold text-xl">{price}</span>&nbsp;
+            <span className="text-sm font-semibold">€</span>
+          </div>
+        </div>
+        <div className="p-4 border-t border-b text-xs text-gray-700">
+          <span className="flex items-center mb-1">
+            <i className="far fa-clock fa-fw mr-2 text-gray-900"></i>
+            <span className="font-bold">Quantity : </span> {availableQuantity}
+          </span>
+          <span className="flex items-center mb-1">
+            <i className="far fa-clock fa-fw mr-2 text-gray-900"></i>{" "}
+            <span className="font-bold"> Seller Name : </span> {instructorName}
+          </span>
+          <span className="flex items-center">
+            <i className="far fa-address-card fa-fw text-gray-900 mr-2"></i>{" "}
+            <span className="font-bold">Email : </span>
+            {instructorEmail}
+          </span>
+        </div>
+        <div className="p-4 flex items-center text-sm text-gray-600">
+          <Rating initialRating={rating} readonly />
+          <span className="ml-2">34 people</span>
+        </div>
+        {user && user.email === instructorEmail && (
+          <div className="p-4 flex  items-center justify-end">
+            <Link to={`/toy/${_id}`} className="btn btn-primary mr-4">
+              details{" "}
+            </Link>
+            <Link to={`/edit/${_id}`} className="btn btn-secondary mr-4">
+              Update{" "}
+            </Link>
+            <button onClick={() => handleDelete(_id)} className="btn btn-error">
+              Delete
+            </button>{" "}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ClassCard;
--- src/pages/Dashboard/InstructorClass/InstructorClass.jsx
+++ src/pages/Dashboard/InstructorClass/InstructorClass.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+import ClassCard from "../../../Components/ClassCard/ClassCard";
+import useAuth from "../../../hooks/useAuth";
+
 const InstructorClass = () => {
+     const { user } = useAuth()
+
+    const FilterByMail =[]
+    //     cateID.filter(
+    //    (toys) => user.email === toys.sellerEmail
+    //  );
+  
+    const myClassFilter = [3, 3]
+      const [myToysFilter, setMyToysFilter] = useState(FilterByMail); 
+      const handleDeleteFilter = (id) => {
+        const deleteFilter = myToysFilter.filter((data) => data._id !== id);
+        setMyToysFilter(deleteFilter);
+      }; 
     return (
-        <div>
-            
-        </div>
+      <div>
+        <body className="antialiased bg-gray-200 text-gray-900 font-sans p-6">
+          <h1 className="mb-12  text-4xl font-extrabold leading-none tracking-tight text-gray-900  text-center md:text-5xl lg:text-6xl ">
+            My Class{" "}
+          </h1>
+          <div className="container mx-auto">
+            <div className="flex flex-wrap -mx-4">
+              {myClassFilter.map((product) => (
+                  <ClassCard
+                    key={product._id}
+                    product={product}
+                    handleDeleteFilter={handleDeleteFilter}
+                  ></ClassCard>
+                ))}
+            </div>
+          </div>
+        </body>
+      </div>
     );
 };
 
