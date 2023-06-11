fc4b66fd3753f3bb9b3636919d743d630cac07c3
Delwer Hossain <delwerhossain006@gmail.com>
2023-06-11 11:44:25 +0600

    ad to cart class build

--- src/Components/ClassCard/ClassCard.jsx
+++ src/Components/ClassCard/ClassCard.jsx
@@ -3,11 +3,12 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useCart from "../../hooks/useCart";
 
 const ClassCard = ({ product, userCheck }) => {
   const { user } = useAuth();
-  // const [, refetch] = useCart();
-  // const navigate = useNavigate();
+  const [, refetch] = useCart();
+  const navigate = useNavigate();
   const location = useLocation();
   const [axiosSecure] = useAxiosSecure()
 
@@ -28,54 +29,51 @@ const ClassCard = ({ product, userCheck }) => {
     } = product;
 
   const handleAddToCart = (item) => {
-  //   console.log(item);
-  //   if (user && user.email) {
-  //     const cartItem = {
-  //       menuItemId: _id,
-  //       name,
-  //       image,
-  //       price,
-  //       email: user.email,
-  //     };
-  //     fetch("https://bistro-boss-server-fawn.vercel.app/carts", {
-  //       method: "POST",
-  //       headers: {
-  //         "content-type": "application/json",
-  //       },
-  //       body: JSON.stringify(cartItem),
-  //     })
-  //       .then((res) => res.json())
-  //       .then((data) => {
-  //         if (data.insertedId) {
-  //           refetch(); // refetch cart to update the number of items in the cart
-  //           Swal.fire({
-  //             position: "top-end",
-  //             icon: "success",
-  //             title: "Food added on the cart.",
-  //             showConfirmButton: false,
-  //             timer: 1500,
-  //           });
-  //         }
-  //       });
-  //   } else {
-  //     Swal.fire({
-  //       title: "Please login to order the food",
-  //       icon: "warning",
-  //       showCancelButton: true,
-  //       confirmButtonColor: "#3085d6",
-  //       cancelButtonColor: "#d33",
-  //       confirmButtonText: "Login now!",
-  //     }).then((result) => {
-  //       if (result.isConfirmed) {
-  //         navigate("/login", { state: { from: location } });
-  //       }
-  //     });
-  //   }
+    console.log(item);
+    if (userCheck) {
+      const cartItem = {
+        classId: _id,
+        name,
+        pictureURL,
+        price,
+        email: user.email,
+      };
+      axiosSecure.post("http://localhost:5000/carts", {cartItem})
+        .then((data) => {
+          if (data.data.insertedId) {
+            refetch(); // refetch cart to update the number of items in the cart
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Food added on the cart.",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
+    } else {
+      Swal.fire({
+        title: "Please login to order the food",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login now!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location } });
+        }
+      });
+    }
   };
 
 
 // delete class - instructor 
-  const handleDelete = (id) => {
+  const handleDelete = (id, userCheck) => {
+    // extra security layer for deleting
+    if (userCheck) {
+      return alert('you have no permission to delete')
+    }
     Swal.fire({
       title: "Are you sure?",
       text: "You will not be able to undo this action.",
@@ -172,7 +170,7 @@ const ClassCard = ({ product, userCheck }) => {
                 Update{" "}
               </Link>
               <button
-                onClick={() => handleDelete(_id)}
+                onClick={() => handleDelete(_id, userCheck)}
                 className="btn btn-error"
               >
                 Delete
