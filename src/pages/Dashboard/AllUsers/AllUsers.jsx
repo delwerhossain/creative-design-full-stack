b2d4eceee87046b3012fa27455916740ba4e6440
Delwer Hossain <delwerhossain006@gmail.com>
2023-06-12 02:32:18 +0600

    remove unused code

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -14,38 +14,11 @@ const AllUsers = () => {
   useEffect(() => {
     AllUsers();
   }, []);
-  // how does reduce work!!!
-  const total = cart.reduce((sum, item) => parseFloat(item.price) + sum, 0);
-
-  const handleDelete = (item) => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        fetch(`http://localhost:5000/users/${item._id}`, {
-          method: "DELETE",
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.deletedCount > 0) {
-              Swal.fire("Deleted!", "Your file has been deleted.", "success");
-            }
-          });
-      }
-    });
-  };
 
   return (
     <div className="w-full">
       <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
-        <h3 className="text-3xl">Total Items: {cart.length}</h3>
-        <h3 className="text-3xl">Total Price: ${total}</h3>
+        <h3 className="text-3xl font-semibold">Total Users: {cart.length}</h3>
         <Link to="/dashboard/payment">
           <button className="btn btn-warning btn-sm">PAY</button>
         </Link>
